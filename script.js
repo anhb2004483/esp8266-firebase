6e7e9821a52b264f30e0a5f2cbecdc3d25cc42b1
anhb2004483 <anhb2004483@student.ctu.edu.vn>
2024-10-06 09:44:17 +0700

    Update script.js
    Signed-off-by: anhb2004483 <anhb2004483@student.ctu.edu.vn>

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// Lấy dữ liệu từ Firebase sau khi người dùng đăng nhập thành công
+// Import Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBI8Dfptxa5q3G6_GB1KfJZjWgU0lWEiDI",
+    authDomain: "espgas-d6120.firebaseapp.com",
+    databaseURL: "https://espgas-d6120-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "espgas-d6120",
+    storageBucket: "espgas-d6120.appspot.com",
+    messagingSenderId: "577421708651",
+    appId: "1:577421708651:web:cfc074f1ffb4761c45902e",
+    measurementId: "G-ZRRBCE50GS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Lấy dữ liệu từ Firebase
+const sensorDataContainer = document.getElementById('sensorDataContainer');
 const snRef = ref(database, 'SN1'); // Tham chiếu đến 'SN1'
 
 onValue(snRef, (snapshot) => {
